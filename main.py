c56e28d1ebad91b43c6d8c1fdab8c8fa826e6ec8
roaan19 <141211644+roaan19@users.noreply.github.com>
2025-04-08 11:43:19 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+a=input("enter the value")
+lst=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+
+if (a in lst) :
+    print("its an alphabet")
+else:
+    print("its not an alphabet")
